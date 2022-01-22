0421e5c65511aac73808d0cc23b757e9b7955ba6
Reynaldo Jose <reynaldo.jose@amg-asia.com>
2022-01-22 16:52:47 +0800

    second commit

--- script.js
+++ script.js
@@ -18,3 +18,11 @@ function  yourAge() {
 function reset() {
     document.getElementById('Age').remove();
 }
+
+
+function generateCat() {
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "https://media1.tenor.com/images/a862d2cb92bfbe6213e298871b1e8a9a/tenor.gif?itemid=15805236";
+    div.appendChild(image);
+}
