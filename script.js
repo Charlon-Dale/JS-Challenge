21fc6e71da729c257afc607341c8991037a7fa42
Charlon Dale San Agustin <charlondalesanagustin@gmail.com>
2022-02-01 19:37:26 +0800

    finished challenge 4

--- script.js
+++ script.js
@@ -112,15 +112,15 @@ for (let i = 0; i <all_buttons.length; i++){
 
 console.log(copyAllButtons);
 
-function buttonColorChange(buttonThingy) {
+function ColorChange(buttonThingy) {
     if (buttonThingy.value === 'red'){
         buttonsRed();
     }
     else if (buttonThingy.value === 'green'){
-        buttonsGreen();
+        buttonGreen();
     }
     else if (buttonThingy.value === 'reset'){
-        buttonsColorReset();
+        buttonColorReset();
     }
     else if (buttonThingy.value === 'random'){
         randomColors();
@@ -128,10 +128,34 @@ function buttonColorChange(buttonThingy) {
     
 }
 
-/*function buttonsRed() {
-    for (let i = 0; i < all_buttons.length; i++){
-        all_buttons[1];
+function buttonsRed() {
+    for (let i=0;i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-danger');
     }
 }
-*/
+
+function buttonGreen() {
+    for (let i=0;i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add('btn-success');
+    }
+}
+
+function buttonColorReset() {
+    for (let i=0;i < all_buttons.length; i++){
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(copyAllButtons[i]);
+    }
+}
+
+function randomColors() {
+    let choices = ['btn-primary', 'btn-danger', 'btn-success', 'btn-warning']
+    
+    for (let i=0;i < all_buttons.length; i++) {
+        let randomNumber = Math.floor(Math.random() * 4); 
+        all_buttons[i].classList.remove(all_buttons[i].classList[1]);
+        all_buttons[i].classList.add(choices[randomNumber]);
+    }
+}
 
